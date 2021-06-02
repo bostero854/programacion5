28ac1b2ed2c34f878ecb5cf1389ac644d1dab790
bostero854 <bostero854@gmail.com>
2021-06-02 01:17:02 -0300

    Cambio de variables

--- Api/Models/Persona.cs
+++ Api/Models/Persona.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.Models
 {
     public class Persona
     {
+        [Required]
+        [StringLength(13, MinimumLength = 8, ErrorMessage = "Ingrese como mínimo 8 caracteres.")]
         public string Dni { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Ingrese como mínimo 3 caracteres.")]
         public string Nombre{ get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Ingrese como mínimo 3 caracteres.")]
         public string Apellido{ get; set; }
+        
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Ingrese como mínimo 3 caracteres.")]
         public string Curso { get; set; }
+
+        [Required]
         public Int32 Año { get; set; }
     }
 }
